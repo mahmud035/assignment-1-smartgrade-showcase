38ffda635f4856ddf94ef72aa35e5866e902d954
Md. Mahamudul Hasan <72532498+mahmud035@users.noreply.github.com>
2024-01-12 12:59:35 +0600

    project complete

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ const Footer = () => {
     <footer className="py-6 md:py-8">
       <div className="container mx-auto">
         <p className="text-base text-center text-gray-500">
-          Copyright ©2024 | All rights reserved by Learn with Sumit
+          Copyright ©2024 | All rights reserved by S.M. Model Govt High School
         </p>
       </div>
     </footer>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -14,7 +14,8 @@ const Header = () => {
           />
           <div>
             <h1 className="text-4xl lg:text-[56px] font-bold leading-[1.1] mb-8">
-              The Future of Learning starts with students at the center
+              Neither Smartphone nor Internet, only education can make you the
+              Best.
             </h1>
             <a className="px-5 py-2.5 bg-[#038C61] rounded-[44px]" href="#">
               Learn More
